5811d5bbdf277572d20c297063fbe8cc0df8cd2d
jdacevedol <45047630+jdacevedol@users.noreply.github.com>
2019-02-28 16:12:00 -0500

    Add files via upload

--- laboratorios/lab02/ejercicioEnLinea/Main.java
+++ laboratorios/lab02/ejercicioEnLinea/Main.java
@@ -8,19 +8,17 @@ public class Main{
         String mat;
         System.out.println("introduzca numero");
         num = lec.nextInt();
-        for (int i = 0; i < num; i++){
+        for (int i = -1; i < num; i++){
             System.out.println("introduzca matriz");
             mat = lec.nextLine();
             for(int j = 0; j < mat.length(); j++){
-                System.out.println(mat.charAt(2));
-                if (mat.charAt(i) == '*'){
+                System.out.println(mat.charAt(j));
+                if (mat.charAt(j) == '*'){
                     fil = i;
                     colm = j;
                 }
             }
         }
-        System.out.println(fil);
-        System.out.println(colm);
         nReinas f = new nReinas(num,fil,colm);
         
     }

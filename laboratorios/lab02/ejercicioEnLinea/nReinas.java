ebcbdd2b5d367c80cabe09fcd252b0f4ac53dad4
jdacevedol <45047630+jdacevedol@users.noreply.github.com>
2019-02-28 16:13:17 -0500

    Add files via upload

--- laboratorios/lab02/ejercicioEnLinea/nReinas.java
+++ laboratorios/lab02/ejercicioEnLinea/nReinas.java
@@ -1,51 +1,42 @@
 import java.util.ArrayList;
 
-
 public class nReinas {
     /*
      * r = reinas
      * etapa = columnas
      */
-    int r;
-    int cant = 0;
-    int fil;
-    int colm;
+    int cant;
+
     public nReinas(int rei, int fila,int colmu){
-            fil = fila;
-            colm = colmu;
-            int [] tal = new int[rei];
-            r = rei;
-            mirarPosiciones(tal,0);
+        int [] tal = new int[rei];
+        mirarPosiciones(tal,0,fila,colmu,rei);
     }
-    
-    public boolean mirarPosiciones(int [] tablero, int etapa){
-                if (etapa > r-1){ 
-                return false;
+
+    public void mirarPosiciones(int [] tablero, int etapa,int fil,int colm,int r){
+        if (etapa == r){ 
+            boolean llave = true;
+
+            for (int j=0; j < r; j++){
+                if (j == colm && tablero[j] == fil){
+                    llave = false;
                 }
-                boolean ex = false;
-                tablero[etapa] = -1;
-                for (int i = 0; i < r; i++){
-                    tablero[etapa] = tablero[etapa] + 1;
-                    if(i != fil && etapa != colm){
-                        if (validarPosicion(tablero,etapa)){
-                            if (etapa != r-1){
-                                ex = mirarPosiciones(tablero,etapa+1);                          
-                       
-                            }else{
-                                cant ++;
-                                imprimirTal(tablero);
-                                System.out.println(cant);
-                                return true;
-                            }
-                        }
-                    }else{
-                        System.out.println("1s");
-                        System.out.println(etapa);
-                    }
+            }
+            if (llave){
+                imprimirTal(tablero);
+                cant++;
+                System.out.println(cant);
+            }
+        }
+        if(etapa < r){
+            for (int i = 0; i < r; i++){
+                tablero[etapa] = i;
+                if (validarPosicion(tablero,etapa)){
+                    mirarPosiciones(tablero,etapa+1,fil,colm,r);  
                 }
-          return ex;
-       }
-       
+            }
+        }
+    }
+
     public boolean validarPosicion(int [] tablero,int etapa){
         for(int i = 0; i < etapa; i++){
             if (tablero[i] == tablero[etapa]  || Math.abs(tablero[i] - tablero[etapa]) == Math.abs(i-etapa)){
@@ -54,12 +45,12 @@ public class nReinas {
         }
         return true;
     }
-    
+
     public void imprimirTal(int [] tal){
-            for(int i = 0; i < tal.length; i++){
-                System.out.print(tal[i]);
-            }
-            System.out.println();
+        for(int i = 0; i < tal.length; i++){
+            System.out.print(tal[i]);
+        }
+        System.out.println();
     }
 
 }

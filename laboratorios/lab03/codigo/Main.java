13fab5d450a12f03aa2dcc9db70e5c954423fc53
ajospino <46940923+ajospino@users.noreply.github.com>
2019-03-20 20:16:41 -0500

    Add files via upload

---
+++ laboratorios/lab03/codigo/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        Punto1_2 r = new Punto1_2();
+        r.Punto1_2(a);
+    }
+}
---
+++ laboratorios/lab03/codigo/Punto1_1.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class Punto1_1 {
+    /**
+     * Metodo que recorre el grafo por medio de dfs teniendo en cuenta que
+     * se quiere encontrar el de menor costo
+     * @param g grafo dado
+     * @param v nodo desde el cual empieza el recorrido
+     * @param w nodo donde termina el recorrido
+     * @return cual es el costo que tiene ir desde inicio a fin
+     */
+    public static int costoMinimo(Digraph g, int v, int w, boolean[] visitados) {
+        visitados[v] = true;
+        if (v == w)
+            return 0;
+        else{
+            ArrayList<Integer> adyacentes = g.getSuccessors(v);
+            if (adyacentes.contains(w))
+                return g.getWeight(v,w);
+            for (Integer adyacente: adyacentes){
+                if (!visitados[adyacente]){
+                    int respuesta = costoMinimo(g, adyacente, w, visitados);
+                    int respuestaConLoQueYaTengo = respuesta + g.getWeight(v,adyacente);
+                    if (respuestaConLoQueYaTengo < g.getWeight(v,w))
+                        return respuestaConLoQueYaTengo;
+
+                }
+            }
+
+            return Integer.MAX_VALUE; //infinito
+        }
+    }
+
+
+
+}

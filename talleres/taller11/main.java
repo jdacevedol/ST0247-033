498169c9d056faf1a8959dd71091ddd5990ecf31
jdacevedol <45047630+jdacevedol@users.noreply.github.com>
2019-05-09 22:38:27 -0500

    Delete main.java

--- talleres/taller11/main.java
+++
@@ -1,6 +0,0 @@
-public class main{
-    public static void main(String[] args) {
-        Taller10 lcs = new Taller10("ABCBDAB","BDCABA");
-        System.out.println("Para ABCBDAB y BDCABA: " + lcs;
-    }
-}

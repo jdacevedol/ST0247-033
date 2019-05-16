4090375bcb12c1177f81260afb0c204a48089f17
jdacevedol <45047630+jdacevedol@users.noreply.github.com>
2019-05-16 13:55:20 -0500

    Update Lector.java

--- proyecto/codigo/Lector.java
+++ proyecto/codigo/Lector.java
@@ -4,6 +4,8 @@ import java.util.LinkedList;
 
 public class Lector{  
     HashMap<Integer, LinkedList<Vertice>>mapaGrafo = new HashMap<Integer, LinkedList<Vertice>>();
+    LinkedList<Carro> car = new LinkedList<Carro>();
+    int recor;
 
     public Lector(String archivo){
       try{
@@ -30,6 +32,7 @@ public class Lector{
             ver = new Vertice(ini,fin,cost);
             hash(ini, ver);
       }
+      proceso();
       }catch(Exception e){
           System.out.println(e);
       }
@@ -41,11 +44,69 @@ public class Lector{
         }else{
           mapaGrafo.put(key, new LinkedList <Vertice>());
           mapaGrafo.get(key).add(ver);
-          System.out.println(mapaGrafo.get(key).get(0).ini);
+          //System.out.println(mapaGrafo.get(key).get(0).ini);
         }
     }
-    
+
+    public int fin(){
+         return 205/5;
+    }
+
     public void proceso(){
-        
+        //list.addLast(val);
+        int max = 0;
+        int min = 100;
+        int cont = 0;
+        for (int i = 0; i<mapaGrafo.get(1).size(); i++) {
+            if (mapaGrafo.get(1).get(i).cost > max){
+              max = mapaGrafo.get(1).get(i).cost;
+            }else if(mapaGrafo.get(1).get(i).cost < min){
+              min = mapaGrafo.get(1).get(i).cost;
+            }
+            cont += mapaGrafo.get(1).get(i).cost; 
+        }
+
+        int prom = cont/205;
+         SacarCarros(max,prom);
+    }
+
+    public void SacarCarros(int max, int prom){
+          Carro carrito ;
+          int act;
+          int ini = 1;
+          int list = -1;
+          //System.out.println(fin());
+            for(int i=0; i < mapaGrafo.get(ini).size(); i++){
+                if(mapaGrafo.get(ini).get(i).fin == -1){
+                    
+                }else if (mapaGrafo.get(ini).get(i).cost > prom ){
+                    carrito = new Carro(mapaGrafo.get(ini).get(i).fin,mapaGrafo.get(ini).get(i).fin);
+                    car.add(carrito);
+                    list += 1; 
+                    act = mapaGrafo.get(ini).get(i).fin;
+                    buscarRecor(list,act,prom);  
+                }
+                
+            }
+         System.out.println("Resultado " + car.size());   
+          
+    }
+
+    public void buscarRecor(int list, int act,int prom){
+        int aux = act;
+        for(int j=0; j < mapaGrafo.get(act).size(); j++){
+              aux = act;
+              if (mapaGrafo.get(act).get(j).cost < prom ){
+                  car.get(list).agre(mapaGrafo.get(act).get(j).fin);
+                  act = mapaGrafo.get(act).get(j).fin;
+                  j = 0;
+                  mapaGrafo.get(1).get(aux).fin = -1;
+              }
+              if(car.get(list).cap == 5){
+                  mapaGrafo.get(1).get(act).fin = -1;
+                  break;
+              }
+        }
+        //map.remove(objeto);
     }
 } 

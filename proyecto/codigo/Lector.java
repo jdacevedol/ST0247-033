3d6793a4b1cec34b5362e832a98c4f1479883da5
ajospino <46940923+ajospino@users.noreply.github.com>
2019-05-21 18:12:55 -0500

    Add files via upload

--- proyecto/codigo/Lector.java
+++ proyecto/codigo/Lector.java
@@ -4,58 +4,68 @@ import java.util.LinkedList;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
+import java.lang.Runtime;
 
-public  class Lector{  
-    
-    HashMap<Integer, LinkedList<Vertice>>mapaGrafo = new HashMap<Integer, LinkedList<Vertice>>();
-    LinkedList<Carro> car = new LinkedList<Carro>();
-    int recor;
+public class Lector{
+
+    HashMap<Integer, LinkedList<Vertice>>mapaGrafo = new HashMap<>();
+    LinkedList<Carro> car = new LinkedList<>();
+    Runtime r = Runtime.getRuntime();
+    long dis ;
+    long usa ;
+    int coor = 4;
 
     public Lector(){
-      try{
-      BufferedReader b = new BufferedReader(new FileReader("C://Users//jdacevedol//Downloads//dataset-ejemplo-U=205-p=1.1.txt"));
-          
-      int ini;
-      int fin;
-      int cost;
-      b.readLine();
-      String text;
-      boolean llave = false; 
-      while(llave != true){
-         text = b.readLine();
-         if (text.equals("Costo de Caminos Cortos. Formato: ID, ID, peso")){
-             break;
-         } 
-      }
-      b.readLine();
-      Vertice ver;
-      while((text = b.readLine()) != null) {
-            String [] parte = text.split(" ");
-            ini = Integer.parseInt(parte[0]);
-            fin = Integer.parseInt(parte[1]);
-            cost = Integer.parseInt(parte[2]);
-            ver = new Vertice(ini,fin,cost);
-            hash(ini, ver);
-      }
-      proceso();
-      }catch(Exception e){
-          System.out.println(e);
-      }
+        try{
+            BufferedReader b = new BufferedReader(new FileReader("4coor_1.7.txt"));
+
+            int ini;
+            int fin;
+            int cost;
+            b.readLine();
+            String text;
+            boolean llave = false;
+            while(llave != true){
+                text = b.readLine();
+                if (text.equals("Costo de Caminos Cortos. Formato: ID, ID, peso")){
+                    break;
+                }
+            }
+            b.readLine();
+            Vertice ver;
+            while((text = b.readLine()) != null) {
+                String [] parte = text.split(" ");
+                ini = Integer.parseInt(parte[0]);
+                fin = Integer.parseInt(parte[1]);
+                cost = Integer.parseInt(parte[2]);
+                ver = new Vertice(ini,fin,cost);
+                hash(ini, ver);
+            }
+            //long start = System.nanoTime();
+            proceso();
+           /** long elapsedTime = System.nanoTime() - start;
+            double billi = Math.pow(10.0,12.0);
+            System.out.println(elapsedTime/billi);
+            */
+        }catch(Exception e){
+            System.out.println(e);
+        }
     }
 
     public void hash(int key, Vertice ver){
         if(mapaGrafo.get(key) != null){
             mapaGrafo.get(key).add(ver);
         }else{
-          mapaGrafo.put(key, new LinkedList <Vertice>());
-          mapaGrafo.get(key).add(ver);
-          //System.out.println(mapaGrafo.get(key).get(0).ini);
+            mapaGrafo.put(key, new LinkedList <Vertice>());
+            mapaGrafo.get(key).add(ver);
+            //System.out.println(mapaGrafo.get(key).get(0).ini);
         }
     }
 
-    public int fin(){
-         return 205/5;
+    /**public int fin(){
+        return coor/5;
     }
+     */
 
     public void proceso(){
         //list.addLast(val);
@@ -64,72 +74,73 @@ public  class Lector{
         int cont = 0;
         for (int i = 0; i<mapaGrafo.get(1).size(); i++) {
             if (mapaGrafo.get(1).get(i).cost > max){
-              max = mapaGrafo.get(1).get(i).cost;
+                max = mapaGrafo.get(1).get(i).cost;
             }else if(mapaGrafo.get(1).get(i).cost < min){
-              min = mapaGrafo.get(1).get(i).cost;
+                min = mapaGrafo.get(1).get(i).cost;
             }
-            cont += mapaGrafo.get(1).get(i).cost; 
+            cont += mapaGrafo.get(1).get(i).cost;
         }
 
-        int prom = cont/205;
-         SacarCarros(max,prom);
+        int prom = cont/coor;
+        SacarCarros(prom);
     }
 
-    public void SacarCarros(int max, int prom){
-          Carro carrito ;
-          int act;
-          int ini = 1;
-          int list = -1;
-          //System.out.println(fin());
-            for(int i=0; i < mapaGrafo.get(ini).size(); i++){
-                if(mapaGrafo.get(ini).get(i).fin == -1){
-                    
-                }else if (mapaGrafo.get(ini).get(i).cost > prom ){
-                    carrito = new Carro(mapaGrafo.get(ini).get(i).fin,mapaGrafo.get(ini).get(i).fin);
-                    car.add(carrito);
-                    list += 1; 
-                    act = mapaGrafo.get(ini).get(i).fin;
-                    buscarRecor(list,act,prom);  
-                }
-                
+    public void SacarCarros(int prom){
+        Carro carrito ;
+        int act;
+        int ini = 1;
+        int list = -1;
+        //System.out.println(fin());
+        for(int i=0; i < mapaGrafo.get(ini).size(); i++){
+            if(mapaGrafo.get(ini).get(i).fin == -1){
+
+            }else if (mapaGrafo.get(ini).get(i).cost > prom ){
+                carrito = new Carro(mapaGrafo.get(ini).get(i).fin,mapaGrafo.get(ini).get(i).fin);
+                car.add(carrito);
+                list += 1;
+                act = mapaGrafo.get(ini).get(i).fin;
+                buscarRecor(list,act,prom);
             }
-         System.out.println("Resultado " + car.size());   
-         PDF();
-          
+
+        }
+        System.out.println("Resultado " + car.size());
+        PDF();
+
     }
 
     public void buscarRecor(int list, int act,int prom){
-        int aux = act;
+        int aux ;
         for(int j=0; j < mapaGrafo.get(act).size(); j++){
-              aux = act;
-              if (mapaGrafo.get(act).get(j).cost < prom ){
-                  car.get(list).agre(mapaGrafo.get(act).get(j).fin);
-                  act = mapaGrafo.get(act).get(j).fin;
-                  j = 0;
-                  mapaGrafo.get(1).get(aux).fin = -1;
-              }
-              if(car.get(list).cap == 5){
-                  mapaGrafo.get(1).get(act).fin = -1;
-                  break;
-              }
+            aux = act;
+            if (mapaGrafo.get(act).get(j).cost < prom ){
+                car.get(list).agre(mapaGrafo.get(act).get(j).fin);
+                act = mapaGrafo.get(act).get(j).fin;
+                j = 0;
+                mapaGrafo.get(1).get(aux).fin = -1;
+            }
+            if(car.get(list).cap == 5){
+                mapaGrafo.get(1).get(act).fin = -1;
+                break;
+            }
         }
         //map.remove(objeto);
+
     }
 
     public void PDF()
     {
-      
+
         try
         {
-            
-            String ruta="C://Users//jdacevedol//Documents";
+
+            //String ruta="C://Users//jdacevedol//Documents";
             FileOutputStream archivo =new FileOutputStream("Carros.pdf");
             Document doc=new Document();
             PdfWriter.getInstance(doc,archivo);
 
             //Image imagen=Image.getInstance("LOGO.USUARIO.png");
             //doc.add(imagen);
-              
+
             doc.open();
             doc.add(new Paragraph("Carros"));
             doc.add(new Paragraph(" "));
@@ -141,8 +152,14 @@ public  class Lector{
             }
             doc.close();
         }catch(Exception e)
-                {
-                    System.out.println("No puede generar el pdf");
-                }
+        {
+            System.out.println("No puede generar el pdf");
+        }
     }
-} 
+
+    public long getMemoria(){
+        dis = r.freeMemory();
+        usa = r.totalMemory();
+        return (usa-dis)/1000000;
+    }
+}
